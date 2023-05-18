b2b664c1775dea4c58010ce551ef46be8f49e5f0
melina611 <132465720+melina611@users.noreply.github.com>
2023-05-18 11:55:08 +0300

    Update hiddenButton.js

--- hiddenButton.js
+++ hiddenButton.js
@@ -1,34 +1,37 @@
-let winnOrLose 
+let winnOrLose
+
 function hiddenButton() {
     winnOrLose = Math.floor(Math.random() * 3)
     let buttonGroup = document.getElementById("buttonGroup")
     for (let button of buttonGroup.getElementsByTagName("button")) {
-        button.addEventListener("click", function (e) {
-            if ("winnerOrLoser" + winnOrLose == button.id) {
-                button.innerHTML = "Winner"
-            } else {
-                button.innerHTML = "Loser"
-            }
-        })
+        button.addEventListener("click", theWinnOrLoseButton)
     }
 }
+
+let randomWinnOrLoseButton
+
 function givenButton() {
     let numberOfProvidedButtons = document.getElementById("numberOfProvidedButtons").value
-    let randomWinnOrLoseButton = Math.floor(Math.random() * numberOfProvidedButtons)
+    randomWinnOrLoseButton = Math.floor(Math.random() * numberOfProvidedButtons)
     let buttons = []
     let outputButtons = document.getElementById("outputButtons")
     outputButtons.innerHTML = ""
     for (let i = 0; i < numberOfProvidedButtons; ++i) {
         buttons[i] = document.createElement("button")
         buttons[i].innerText = "Winn or Lose"
+        buttons[i].id = i
         outputButtons.appendChild(buttons[i])
-        buttons[i].addEventListener("click", function (e) {
-            if (i == randomWinnOrLoseButton) {
-                buttons[i].innerText = "Winner"
-            } else {
-                buttons[i].innerText = "Loser"
-            }
-        })
+        buttons[i].addEventListener("click", theWinnOrLoseButton)
+    }
+}
+
+function theWinnOrLoseButton(e) {
+    if (e.target.id == randomWinnOrLoseButton) {
+        e.target.innerText = "Winner"
+    } else if ("winnerOrLoser" + winnOrLose == e.target.id) {
+        e.target.innerHTML = "Winner"
+    } else {
+        e.target.innerText = "Loser"
     }
 }
 window.onload = hiddenButton
